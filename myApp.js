bd3adf0fb4ee6709ef24eae27b4f0b87315c6593
tjaqu001 <tjauq001@ucr.edu>
2024-01-30 23:09:08 +0800

    syntax fix

--- myApp.js
+++ myApp.js
@@ -22,7 +22,7 @@ app.use(helmet.noCache());
 
 app.use(helmet.contentSecurityPolicy({
   directives: {
-    defaultSrc: ["'self"],
+    defaultSrc: ["'self'"],
     scriptSrc: ["'self'", 'trusted-cdn.com']
   }
 }));
