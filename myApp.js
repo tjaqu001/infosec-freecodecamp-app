92aeb14c491fbcfc1b3f0c75ae695e41be1d6324
tjaqu001 <tjauq001@ucr.edu>
2024-01-30 21:37:26 +0800

    set content security policy

--- myApp.js
+++ myApp.js
@@ -20,6 +20,13 @@ app.use(helmet.dnsPrefetchControl());
 
 app.use(helmet.noCache());
 
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self"],
+    scriptSrc: ["'self'", "trusted-cdn.com"]
+  }
+}));
+
 
 
 
